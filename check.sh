94183df48bc64d18aedf6970ba38d18ac66c7f9f
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2017-10-10 00:24:05 +0200

    First try
    TODO:
    * Fix test15.liq
    * Remove ("..." : tez)

--- check.sh
+++ check.sh
@@ -18,3 +18,6 @@ echo ./_obuild/${LIQUIDITY}/${LIQUIDITY}.asm tests/$test.liq.tz.liq
 ./_obuild/${LIQUIDITY}/${LIQUIDITY}.asm tests/$test.liq.tz.liq || exit 2
 
 ./tezos/_obuild/tezos-client/tezos-client.asm typecheck program tests/$test.liq.tz.liq.tz
+
+echo ./_obuild/ocp-liquidity-comp/ocp-liquidity-comp.asm -I +../zarith zarith.cma -I _obuild/liquidity-env ./_obuild/liquidity-env/liquidity-env.cma -impl tests/$test.liq
+./_obuild/ocp-liquidity-comp/ocp-liquidity-comp.asm -I +../zarith zarith.cma -I _obuild/liquidity-env ./_obuild/liquidity-env/liquidity-env.cma -dsource -impl tests/$test.liq
---
+++ tools/liquidity/ocaml/liquidOCamlMain.ml
@@ -0,0 +1,347 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+module Compile = LiquidOCamlCompile
+
+let clean_ast =
+  let open Asttypes in
+  let open Longident in
+  let open Parsetree in
+  let open Ast_mapper in
+  let open Ast_helper in
+  {
+    default_mapper with
+    structure_item = (fun mapper item ->
+    match item with
+    | { pstr_desc =
+          Pstr_extension
+            (({ Asttypes.txt = "version" },
+              PStr [{ pstr_desc = Pstr_eval (exp,[])}]),[])
+      } ->
+       Str.eval (Exp.constant (Const.int 0))
+    | { pstr_desc =
+          Pstr_extension
+            (({ Asttypes.txt = "entry" },
+              PStr [entry]),[])
+      } ->
+       mapper.structure_item mapper entry
+    | _ ->
+       default_mapper.structure_item mapper item
+  );
+    expr = (fun mapper expr ->
+      let loc = expr.pexp_loc in
+      match expr.pexp_desc with
+      | Pexp_constant (
+                  Pconst_integer (s, Some 'p')
+                | Pconst_integer (s, None)
+              )
+         ->
+           Exp.apply ~loc (Exp.ident ~loc
+                        { txt = Ldot(Lident "Int",
+                                     "of_string"); loc })
+                     [Nolabel, Exp.constant ~loc (Pconst_string (s,None))]
+      | Pexp_constant (
+                  Pconst_integer (s, Some '\231')
+              )
+        ->
+         Exp.apply ~loc (Exp.ident ~loc
+                      { txt = Ldot(Lident "Tez",
+                            "of_string"); loc })
+                    [Nolabel, Exp.constant ~loc (Pconst_string (s,None))]
+      | Pexp_constant (
+                  Pconst_float (s, Some '\231')
+              )
+        ->
+         Exp.apply ~loc (Exp.ident ~loc
+                      { txt = Ldot(Lident "Tez",
+                            "of_string"); loc })
+                    [Nolabel, Exp.constant ~loc (Pconst_string (s,None))]
+
+      | Pexp_construct (
+              { txt = Lident "Map" }, None
+              )
+        ->
+         Exp.apply ~loc (Exp.ident ~loc
+                      { txt = Ldot(Lident "Map",
+                            "empty"); loc })
+                   [Nolabel, Exp.constant ~loc (Pconst_integer ("0",None))]
+
+      | Pexp_construct (
+              { txt = Lident "Map" }, Some list
+              )
+        ->
+         let list = default_mapper.expr mapper list in
+         Exp.apply ~loc (Exp.ident ~loc
+                      { txt = Ldot(Lident "Map",
+                            "make"); loc })
+                   [Nolabel, list]
+
+
+      | Pexp_construct (
+              { txt = Lident "Set" }, None
+              )
+        ->
+         Exp.apply ~loc (Exp.ident ~loc
+                      { txt = Ldot(Lident "Set",
+                            "empty"); loc })
+                   [Nolabel, Exp.constant ~loc (Pconst_integer ("0",None))]
+
+      | Pexp_construct (
+              { txt = Lident "Set" }, Some list
+              )
+        ->
+         let list = default_mapper.expr mapper list in
+         Exp.apply ~loc (Exp.ident ~loc
+                      { txt = Ldot(Lident "Set",
+                            "make"); loc })
+                   [Nolabel, list]
+
+      | Pexp_setfield (record, label1, value) ->
+         let value = default_mapper.expr mapper value in
+         begin
+           match record.pexp_desc with
+           | Pexp_field (record, label2) ->
+              let record = default_mapper.expr mapper record in
+              let id = "record#" in
+              Exp.let_
+                ~loc Nonrecursive
+                [Vb.mk ~loc (Pat.var ~loc { txt = id; loc }) record]
+                (Exp.record
+                   ~loc
+                   [label2,
+                    Exp.record
+                      ~loc [
+                        label1, value
+                      ]
+                      (Some (Exp.field ~loc (Exp.ident ~loc
+                                                       { txt = Lident id; loc})
+                                       label2))
+                   ]
+                   (Some (Exp.ident ~loc { txt = Lident id; loc}))
+                )
+           | _ ->
+              let record = default_mapper.expr mapper record in
+              Exp.record ~loc [label1, value] (Some record)
+         end
+
+      | _ -> default_mapper.expr mapper expr
+    );
+    (* `String of int * string` is compiled as `String of (int * string)` *)
+    constructor_declaration = (fun mapper pcd ->
+      match pcd.pcd_args with
+      | Pcstr_tuple []
+      | Pcstr_tuple [_] -> pcd
+      | Pcstr_tuple list -> { pcd with pcd_args =
+                                         Pcstr_tuple [Typ.tuple list] }
+      | _ -> pcd
+    );
+  }
+
+
+let () =
+  let open Ast_mapper in
+  LiquidOCamlPparse.ImplementationHooks.add_hook
+    "liquid" (fun hook_info ast ->
+      clean_ast.structure clean_ast ast
+      )
+
+
+open Clflags
+open Compenv
+
+let usage = "Usage: ocamlc <options> <files>\nOptions are:"
+
+(* Error messages to standard error formatter *)
+let ppf = Format.err_formatter
+
+let show_config () =
+  Config.print_config stdout;
+  exit 0;
+;;
+
+module Options = Main_args.Make_bytecomp_options (struct
+  let set r () = r := true
+  let unset r () = r := false
+  let _a = set make_archive
+  let _absname = set Location.absname
+  let _annot = set annotations
+  let _binannot = set binary_annotations
+  let _c = set compile_only
+  let _cc s = c_compiler := Some s
+  let _cclib s = Compenv.defer (ProcessObjects (Misc.rev_split_words s))
+  let _ccopt s = first_ccopts := s :: !first_ccopts
+  let _compat_32 = set bytecode_compatible_32
+  let _config = show_config
+  let _custom = set custom_runtime
+  let _no_check_prims = set no_check_prims
+  let _dllib s = defer (ProcessDLLs (Misc.rev_split_words s))
+  let _dllpath s = dllpaths := !dllpaths @ [s]
+  let _for_pack s = for_package := Some s
+  let _g = set debug
+  let _i () = print_types := true; compile_only := true
+  let _I s = include_dirs := s :: !include_dirs
+  let _impl = impl
+  let _intf = intf
+  let _intf_suffix s = Config.interface_suffix := s
+  let _keep_docs = set keep_docs
+  let _no_keep_docs = unset keep_docs
+  let _keep_locs = set keep_locs
+  let _no_keep_locs = unset keep_locs
+  let _labels = unset classic
+  let _linkall = set link_everything
+  let _make_runtime () =
+    custom_runtime := true; make_runtime := true; link_everything := true
+  let _alias_deps = unset transparent_modules
+  let _no_alias_deps = set transparent_modules
+  let _app_funct = set applicative_functors
+  let _no_app_funct = unset applicative_functors
+  let _noassert = set noassert
+  let _nolabels = set classic
+  let _noautolink = set no_auto_link
+  let _nostdlib = set no_std_include
+  let _o s = output_name := Some s
+  let _opaque = set opaque
+  let _open s = open_modules := s :: !open_modules
+  let _output_obj () = output_c_object := true; custom_runtime := true
+  let _output_complete_obj () =
+    output_c_object := true;
+    output_complete_object := true;
+    custom_runtime := true
+  let _pack = set make_package
+  let _pp s = preprocessor := Some s
+  let _ppx s = first_ppx := s :: !first_ppx
+  let _plugin p = Compplugin.load p
+  let _principal = set principal
+  let _no_principal = unset principal
+  let _rectypes = set recursive_types
+  let _no_rectypes = unset recursive_types
+  let _runtime_variant s = runtime_variant := s
+  let _safe_string = unset unsafe_string
+  let _short_paths = unset real_paths
+  let _strict_sequence = set strict_sequence
+  let _no_strict_sequence = unset strict_sequence
+  let _strict_formats = set strict_formats
+  let _no_strict_formats = unset strict_formats
+  let _thread = set use_threads
+  let _vmthread = set use_vmthreads
+  let _unboxed_types = set unboxed_types
+  let _no_unboxed_types = unset unboxed_types
+  let _unsafe = set fast
+  let _unsafe_string = set unsafe_string
+  let _use_prims s = use_prims := s
+  let _use_runtime s = use_runtime := s
+  let _v () = print_version_and_library "compiler"
+  let _version = print_version_string
+  let _vnum = print_version_string
+  let _w = (Warnings.parse_options false)
+  let _warn_error = (Warnings.parse_options true)
+  let _warn_help = Warnings.help_warnings
+  let _color option =
+    begin match parse_color_setting option with
+          | None -> ()
+          | Some setting -> color := Some setting
+    end
+  let _where = print_standard_library
+  let _verbose = set verbose
+  let _nopervasives = set nopervasives
+  let _dsource = set dump_source
+  let _dparsetree = set dump_parsetree
+  let _dtypedtree = set dump_typedtree
+  let _drawlambda = set dump_rawlambda
+  let _dlambda = set dump_lambda
+  let _dinstr = set dump_instr
+  let _dtimings = set print_timings
+
+  let _args = Arg.read_arg
+  let _args0 = Arg.read_arg0
+
+  let anonymous = anonymous
+end)
+
+let main () =
+  Clflags.add_arguments __LOC__ Options.list;
+  try
+    readenv ppf Before_args;
+    Clflags.parse_arguments anonymous usage;
+    Compmisc.read_color_env ppf;
+    begin try
+      Compenv.process_deferred_actions
+        (ppf,
+         Compile.implementation,
+         Compile.interface,
+         ".cmo",
+         ".cma");
+    with Arg.Bad msg ->
+      begin
+        prerr_endline msg;
+        Clflags.print_arguments usage;
+        exit 2
+      end
+    end;
+    readenv ppf Before_link;
+    if
+      List.length (List.filter (fun x -> !x)
+                      [make_archive;make_package;compile_only;output_c_object])
+        > 1
+    then
+      if !print_types then
+        fatal "Option -i is incompatible with -pack, -a, -output-obj"
+      else
+        fatal "Please specify at most one of -pack, -a, -c, -output-obj";
+    if !make_archive then begin
+      Compmisc.init_path false;
+
+      Bytelibrarian.create_archive ppf
+                                   (Compenv.get_objfiles ~with_ocamlparam:false)
+                                   (extract_output !output_name);
+      Warnings.check_fatal ();
+    end
+    else if !make_package then begin
+      Compmisc.init_path false;
+      let extracted_output = extract_output !output_name in
+      let revd = get_objfiles ~with_ocamlparam:false in
+      Bytepackager.package_files ppf (Compmisc.initial_env ())
+        revd (extracted_output);
+      Warnings.check_fatal ();
+    end
+    else if not !compile_only && !objfiles <> [] then begin
+      let target =
+        if !output_c_object then
+          let s = extract_output !output_name in
+          if (Filename.check_suffix s Config.ext_obj
+            || Filename.check_suffix s Config.ext_dll
+            || Filename.check_suffix s ".c")
+          then s
+          else
+            fatal
+              (Printf.sprintf
+                 "The extension of the output file must be .c, %s or %s"
+                 Config.ext_obj Config.ext_dll
+              )
+        else
+          default_output !output_name
+      in
+      Compmisc.init_path false;
+      Bytelink.link ppf (get_objfiles ~with_ocamlparam:true) target;
+      Warnings.check_fatal ();
+    end;
+  with x ->
+    Location.report_exception ppf x;
+    exit 2
+
+let _ =
+  Timings.(time All) main ();
+  if !Clflags.print_timings then Timings.print Format.std_formatter;
+  exit 0
