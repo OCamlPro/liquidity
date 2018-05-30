9eec1269284988ca0e37848725dbbed8198fab36
Alain Mebsout <alainmebsout@gmail.com>
2018-05-30 16:08:18 +0200

    Tests: Allow arguments to tezos-client in tests

--- config.sh
+++ config.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# TEZOS_FULL_PATH=./tezos/_build/default/src/client_main.exe
-#TEZOS_FULL_PATH=./tezos/_obuild/tezos-client/tezos-client.asm
 TEZOS_FULL_PATH=./tezos/tezos-client
+# TEZOS_ARGS="--addr node.tzscan.io --port 80"
