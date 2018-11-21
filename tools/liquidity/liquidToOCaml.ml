eaf5759a176daf51dc9a92d653a155d5f6870f54
Alain Mebsout <alainmebsout@gmail.com>
2018-11-21 17:21:58 +0100

    Bump version to 0.404

--- tools/liquidity/liquidToOCaml.ml
+++ tools/liquidity/liquidToOCaml.ml
@@ -8,7 +8,7 @@
 (**************************************************************************)
 
 (* The version that will be required to compile the generated files. *)
-let output_version = "0.403"
+let output_version = "0.404"
 
 open Asttypes
 open Longident
