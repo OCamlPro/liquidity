52e0ec06f7899b4f9ab13ddea51e8dc4782e4472
Alain Mebsout <alainmebsout@gmail.com>
2018-12-06 15:16:11 +0100

    Change travis ubuntu repo

--- travis-scripts/prepare-trusty.sh
+++ travis-scripts/prepare-trusty.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y -qq libgmp-dev pandoc # ocaml ocaml-native-compilers
 
 # do this in a second step to only install libsecp256k1-dev libsecp256k1-0
 # for ubuntu, these packages are not available in trusty
-sudo add-apt-repository "deb http://cz.archive.ubuntu.com/ubuntu artful main universe"
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu artful main universe"
 sudo apt-get update -qq
 sudo apt-get install -y -qq \
      libsecp256k1-dev libsecp256k1-0 libsodium-dev libssl-dev \
