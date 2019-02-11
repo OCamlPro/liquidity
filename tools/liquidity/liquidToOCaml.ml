ec2251e0d8821a52360298d1936bda0cd5fbff6b
Alain Mebsout <alainmebsout@gmail.com>
2019-02-11 17:27:26 +0100

    Bump version 0.52 and remove merge trash

--- tools/liquidity/liquidToOCaml.ml
+++ tools/liquidity/liquidToOCaml.ml
@@ -8,7 +8,7 @@
 (**************************************************************************)
 
 (* The version that will be required to compile the generated files. *)
-let output_version = "0.51"
+let output_version = "0.52"
 
 open Asttypes
 open Longident
