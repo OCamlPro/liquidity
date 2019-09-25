12f92d3b9a63cfd269a97261b44cd90626200f4f
Alain Mebsout <alain.mebsout@origin-labs.com>
2019-09-25 18:24:24 +0200

    Bump to version 1.052

--- tools/liquidity/liquidToParsetree.ml
+++ tools/liquidity/liquidToParsetree.ml
@@ -22,7 +22,7 @@
 (****************************************************************************)
 
 (* The version that will be required to compile the generated files. *)
-let output_version = "1.051"
+let output_version = "1.052"
 
 open Asttypes
 open Longident
