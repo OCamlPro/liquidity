0ae728657f37e79154a82a81dc65e24984f33736
Steven de Oliveira <de.oliveira.steven@gmail.com>
2020-05-29 12:13:57 +0200

    Adding full dependencies for interpreter

--- libs/love/love_env.ml
+++
@@ -1,7 +0,0 @@
-type t = unit
-
-let find_sig_opt _ _ _ = assert false
-
-let find_struct_opt _ _ _ = assert false
-
-
--- libs/love/love_translator.ml
+++
@@ -1,5 +0,0 @@
-let make_type_absolute _ _ _ = assert false
-
-let inline_value _ _ = assert false
-
-let rebase_contract ~from_path ~to_path _ _ = assert false
