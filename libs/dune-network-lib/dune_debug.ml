af1f7ace128d7787d1defcf2e836be67e3e0c868
Alain Mebsout <alain.mebsout@origin-labs.com>
2020-06-17 18:38:41 +0200

    dune_debug from dune network sources

--- libs/dune-network-lib/dune_debug.ml
+++ libs/dune-network-lib/dune_debug.ml
@@ -13,35 +13,8 @@
 (*  along with this program.  If not, see <https://www.gnu.org/licenses/>.*)
 (**************************************************************************)
 
-let eprintf x =
-  Format.kfprintf (fun fmt -> Format.fprintf fmt "@.") Format.err_formatter x
-
-let ignore_eprintf x = Format.ifprintf Format.err_formatter x
-
-let debug =
-  match
-    Environment_variable.(
-      get_opt @@ make "DUNE_DEBUG" ~description:"Debug flag for protocol")
-  with
-  | None ->
-      0
-  | Some "n" ->
-      -1
-  | Some s -> (
-    match int_of_string s with exception _ -> 1 | n -> n )
-
-let printf ?(n = 1) fmt =
-  if n <= debug then eprintf fmt else ignore_eprintf fmt
-
-let string_of_exn exn = Printexc.to_string exn
-
-let polymorphic_compare = compare
-
-module Array = struct
-  type 'a t = 'a array
-
-  include Array
+module P2p_peer_id = struct
+  let of_b58check _ = assert false
 end
 
-(* let is_peer_id s =
- *   match P2p_peer_id.of_b58check s with Ok _ -> true | _ -> false *)
+#include "../../dune-network/src/lib_base/dune_debug.ml"
