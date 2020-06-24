69b28ec87d2ca0f43874c9cf2d8b048ce5dfd6de
Alain Mebsout <alain.mebsout@origin-labs.com>
2020-06-24 10:32:31 +0200

    Remove stderr printing from Dune Network config

---
+++ libs/dune-network-lib/config_network.ml
@@ -0,0 +1,29 @@
+(****************************************************************************)
+(*                               Liquidity                                  *)
+(*                                                                          *)
+(*                  Copyright (C) 2017-2020 OCamlPro SAS                    *)
+(*                                                                          *)
+(*                    Authors: Fabrice Le Fessant                           *)
+(*                             Alain Mebsout                                *)
+(*                             David Declerck                               *)
+(*                             Steven De Oliveira                           *)
+(*                                                                          *)
+(*  This program is free software: you can redistribute it and/or modify    *)
+(*  it under the terms of the GNU General Public License as published by    *)
+(*  the Free Software Foundation, either version 3 of the License, or       *)
+(*  (at your option) any later version.                                     *)
+(*                                                                          *)
+(*  This program is distributed in the hope that it will be useful,         *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of          *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *)
+(*  GNU General Public License for more details.                            *)
+(*                                                                          *)
+(*  You should have received a copy of the GNU General Public License       *)
+(*  along with this program.  If not, see <https://www.gnu.org/licenses/>.  *)
+(****************************************************************************)
+
+module Printf = struct
+  let eprintf fmt = Format.ifprintf Format.err_formatter fmt
+end
+
+#include "../../dune-network/src/lib_base/config_network.ml"
